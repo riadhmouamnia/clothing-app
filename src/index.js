a21b4c7a6bcc0285092af9de782774d6130b0011
Riadh mouamnia <riad.sodmg@gmail.com>
2022-08-28 23:05:27 +0100

    update our menu-item to navigate withRouter HOC

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route } from "react-router-dom";
+
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </React.StrictMode>
 );
